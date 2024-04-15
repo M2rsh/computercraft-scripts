323a0f8658985e4b4024c11d2436c9deeba022bc
Che <mariuszbubak10@gmail.com>
2024-04-15 18:21:08 +0200

    Multiple speakers

--- play.lua
+++ play.lua
@@ -1,26 +1,29 @@
 local dfpwm = require("cc.audio.dfpwm")
-local speaker = peripheral.find("speaker")
+local speakers = table.pack(peripheral.find("speaker"))
 local drives = require("drives")
-
-local audio_data = cfs_get("name.dfpwm")
+local audio_data = cfs_get("redsuninthesky.dfpwm")
 
 local decoder = dfpwm.make_decoder()
 
 function linesFromString(str, chunkSize)
-    local index = 1
-    return function()
-        if index <= #str then
-            local chunk = str:sub(index, index + chunkSize - 1)
-            index = index + chunkSize
-            return chunk
-        end
+  local index = 1
+  return function()
+    if index <= #str then
+      local chunk = str:sub(index, index + chunkSize - 1)
+      index = index + chunkSize
+      return chunk
     end
+  end
 end
 
-for chunk in linesFromString(audio_data, 16*1024) do
-    local buffer = decoder(chunk)
-
-    while not speaker.playAudio(buffer) do
+local funcs = {}
+for chunk in linesFromString(audio_data, 16 * 1024) do
+  local buffer = decoder(chunk)
+  for i = 1, #speakers do
+    funcs[i] = function()
+      while not speakers[i].playAudio(buffer) do
         os.pullEvent("speaker_audio_empty")
+      end
     end
+  end
 end
